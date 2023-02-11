71474abcd1059732b9b61af3c985c9607eec50f8
eli.gui <guihai@hyticn.com>
2023-02-11 12:03:03 +0800

    feat: 添加纪念日日期方法

--- src/main/java/cn/jackbin/SimpleRecord/utils/DateUtil.java
+++ src/main/java/cn/jackbin/SimpleRecord/utils/DateUtil.java
@@ -1,6 +1,5 @@
 package cn.jackbin.SimpleRecord.utils;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -15,14 +14,57 @@ import java.util.List;
  * @date: 2020/10/27 22:20
  **/
 public class DateUtil {
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private static final long MS = 1L;
+    private static final long SECOND = MS * 1000;
+    private static final long MIN = SECOND * 60;
+    private static final long HOUR = MIN * 60;
+    private static final long DAY = HOUR * 24;
 
     /**
      * 获取当前时间
      */
     public static String getCurrentTime() {
-        SimpleDateFormat sf= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date date = new Date(System.currentTimeMillis());
         return sf.format(date);
+    }
+
+    /**
+     * 距离下一个纪念日还有几天
+     *
+     * @param anniversary 纪念日日期
+     * @return {@link int}
+     * @author Eli.Gui 2023-02-11 10:11:43
+     */
+    public static int beforeForAnniversary(Date anniversary) {
+        int day = 0;
+        Calendar currentDate = Calendar.getInstance();
+        Calendar anniversaryDate = Calendar.getInstance();
+        anniversaryDate.setTime(anniversary);
+        anniversaryDate.set(Calendar.YEAR, currentDate.get(Calendar.YEAR));
+        long currentInMillis = currentDate.getTimeInMillis();
+        long birthDayInMillis = anniversaryDate.getTimeInMillis();
+        long l = birthDayInMillis - currentInMillis;
+        if (l <= -DAY) {
+            anniversaryDate.add(Calendar.YEAR, 1);
+            birthDayInMillis = anniversaryDate.getTimeInMillis();
+            l = birthDayInMillis - currentInMillis;
+        }
+        long longDay = l / DAY;
+        day = (int) (longDay) + 1;
+        if (0L >= l && l > -DAY) {
+            day = 0;
+        }
+        return day;
+    }
+
+    public static void main(String[] args) throws ParseException {
+        String dateStr = "2019-01-12";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date parse = sdf.parse(dateStr);
+        int before = DateUtil.beforeForAnniversary(parse);
+        System.out.println(before);
     }
 
 
@@ -44,11 +86,11 @@ public class DateUtil {
         Calendar calder1 = Calendar.getInstance();
         calder1.setTime(date1);//设置时间
         int year1 = calder1.get(Calendar.YEAR);//获取年份
-        int month1=calder1.get(Calendar.MONTH);//获取月份
+        int month1 = calder1.get(Calendar.MONTH);//获取月份
         Calendar calder2 = Calendar.getInstance();
         calder2.setTime(date1);//设置时间
         int year2 = calder2.get(Calendar.YEAR);//获取年份
-        int month2=calder2.get(Calendar.MONTH);//获取月份
+        int month2 = calder2.get(Calendar.MONTH);//获取月份
         return year1 == year2 && month1 == month2;
     }
 
@@ -118,8 +160,8 @@ public class DateUtil {
     /**
      * 按照月份分割一段时间
      *
-     * @param startTime     开始时间戳(毫秒)
-     * @param endTime       结束时间戳(毫秒)
+     * @param startTime 开始时间戳(毫秒)
+     * @param endTime   结束时间戳(毫秒)
      */
     public static List<Long> getIntervalTimeByMonth(Date startTime, Date endTime) {
         List<Long> dateList = new ArrayList<>();
@@ -135,7 +177,7 @@ public class DateUtil {
             calendar.set(Calendar.HOUR_OF_DAY, 13);
             calendar.set(Calendar.MINUTE, 59);
             calendar.set(Calendar.SECOND, 59);
-            if(calendar.getTimeInMillis() >= endDate.getTime()){
+            if (calendar.getTimeInMillis() >= endDate.getTime()) {
                 dateList.add(endTime.getTime());
                 break;
             }
@@ -148,16 +190,4 @@ public class DateUtil {
         return dateList;
     }
 
-    public static void main(String[] args) throws ParseException {
-        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        Date beginDate = sdf.parse("2020-07-1");
-        Date endDate = sdf.parse("2020-12-01");
-        List<Long> endDateList = DateUtil.getIntervalTimeByMonth(beginDate, endDate);
-        for (int i = 0; i < endDateList.size(); i++) {
-            Long beginStr = endDateList.get(i);
-            String begin1 = sdf1.format(new Date(beginStr));
-            System.out.println("第" + i + "段时间区间:" + begin1);
-        }
-    }
 }
